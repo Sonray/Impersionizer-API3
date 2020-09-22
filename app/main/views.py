670986456a9c1f417f3d251ee877096e5b9857a9
David Okwacha <osweredavid@gmail.com>
2020-09-22 14:32:52 +0300

    created the sign up form route

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,13 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from .forms import Sign_up_form
 
+#views
+@main.route('/')
+def index():
+    return render_template('index.hmtl')
+
+@main.route('/signup')
+def signup():
+    form = Sign_up_form()
+    return render_template('signup.html', form = form)
