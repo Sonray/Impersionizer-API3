6a4a2257c8eae2bb89b356a8247f6ddeb9c7ee1a
David Okwacha <osweredavid@gmail.com>
2020-09-27 05:31:41 +0300

    Added likes and dislike tables and relationships

--- app/models.py
+++ app/models.py
@@ -10,6 +10,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique = True)
     password = db.Column(db.String(90))
     child = db.relationship('Pitch', backref= 'User', uselist=True )
+    like = db.relationship('Likes', backref= 'likes', uselist=True )
+    dislike = db.relationship('Dislikes', backref= 'dislikes', uselist=True )
 
     def __repr__(self):
         return f'User {self.username, self.email, self.password}'
@@ -21,6 +23,26 @@ class Pitch(UserMixin, db.Model):
     pitch = db.Column(db.String(255))
     category = db.Column(db.String(255))
     User_id = db.Column(db.Integer, db.ForeignKey('user.id') )
+    liked = db.relationship('Likes', backref='liked')
+    disliked = db.relationship('Dislike', backref='disliked')
 
     def __repr__(self):
-        return f'Pitch {self.pitch}'
+        return f'Pitch {self.pitch, self.category }'
+
+class Likes(UserMixin, db.Model):
+    __tablename__ = 'Likes'
+    id = db.Column(db.Integer, primary_key=True)
+    User_id = db.Column(db.Integer, db.ForeignKey('user.id') )
+    Pitch_id = db.Column(db.Integer, db.ForeignKey('Pitches.id') )
+
+    def __repr__(self):
+        return f'Pitch {self.User_id, self.Pitch_id }'
+
+class Dislikes(UserMixin, db.Model):
+    __tablename__ = 'Likes'
+    id = db.Column(db.Integer, primary_key=True)
+    User_id = db.Column(db.Integer, db.ForeignKey('user.id') )
+    Pitch_id = db.Column(db.Integer, db.ForeignKey('Pitches.id') )
+
+    def __repr__(self):
+        return f'Pitch {self.User_id, self.Pitch_id }'
